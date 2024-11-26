0cb92a1fd54b484ac9f402129f8997d39bedad09
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-11-26 09:17:52 +0100

    solution exersice

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class LogIn {
     public static void main(String[] args) {
         //Escribe un programa que pida al usuario ingresar un nombre de usuario y una constraseña, escribe un bucle que siga ejecutándose y devuelva 'Datos incorrectos por favor vuelve a intentar' mientras el nombre de usuario o la contraseña sean incorrectos.
         //Cuando el usuario y la constraseña sean correctos imprime en terminal: ¡Acceso concedido!
 
-        //String correctUsername = "Pepita";
-        //String correctPassword = "pepi123";
+        String correctUsername = "Pepita";
+        String correctPassword = "pepi123";
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nLog In\n");
+        System.out.print("- Usuario: ");
+        String name = scan.nextLine();
+        System.out.print("- Contraseña: ");
+        String pass = scan.nextLine();
+
+
+        while (!name.equals(correctUsername) || !pass.equals(correctPassword)) {
+            System.out.println("\nDatos incorrectos por favor vuelve a intentar");
+            System.out.print("- Usuario: ");
+            name = scan.nextLine();
+            System.out.print("- Contraseña: ");
+            pass = scan.nextLine();
+        }
+        System.out.println("\n¡Acceso concedido!");
+
+        scan.close();
     }
 }
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,25 +1,53 @@
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main(String[] args) {
 
-        //Escribe un bucle while que genere números aleatorios hasta que salga el número 7. Has de usar la función getRandomNumber
+        //Escribe un bucle while que genere números aleatorios hasta que salga el número 7
+        //Has de usar la función getRandomNumber
+
+        int diceRoll = -1;
+        while(diceRoll != 7){
+            diceRoll = getRandomNumber(10);
+            System.out.println(diceRoll);
+        }
 
 
+        //Escribe un bucle while que genere números aleatorios hasta que salga un número mayor a 70
+        //Has de usar la función getRandomNumber
+        int randomNum = 1;
+        while (randomNum < 70) {
+            randomNum = getRandomNumber(100);
+            System.out.println(randomNum);
+        }
 
-        //Escribe un bucle while que genere números aleatorios hasta que salga un número mayor a 70. Has de usar la función getRandomNumber
-        
+        //Escribe un bucle do while que genere un número aleatorio entre 1 y 100 (usando la función getRandomNumber). El usuario debe adivinar el número aleatorio y el programa debe seguir solicitando intentos hasta que el usuario adivine correctamente. Después de cada intento, el programa debe indicar si el número ingresado es mayor o menor que el número generado.
 
+        Scanner scanner = new Scanner(System.in);
+        int secretNumber = getRandomNumber(100);
+        int userChoice;
 
-        //Escribe un bucle do while que genere un número aleatorio entre 1 y 100. El usuario debe adivinar el número aleatorio y el programa debe seguir solicitando intentos hasta que el usuario adivine correctamente. Después de cada intento, el programa debe indicar si el número ingresado es mayor o menor que el número generado. Has de usar la función getRandomNumber
+        do {
+            System.out.print("Adivina el número (entre 1 y 100): ");
+            userChoice = scanner.nextInt();
 
-    
+            if (userChoice < secretNumber) {
+                System.out.println("El número es mayor.");
+            } else if (userChoice > secretNumber) {
+                System.out.println("El número es menor.");
+            }
+        } while (userChoice != secretNumber);
+
+        System.out.println("¡Felicidades! Has adivinado el número.");
+        scanner.close();
     }
 
     /**
      * Function name: getRandomNumber
-     * 
+     *
      * @param number (int)
      * @return (int)
-     * 
+     *
      * Inside the function:
      * 1. choose a random integer between 1 and the number given
      */
